cc6279386c750e8d3dd03198b456ca81c0f06ad8
Sitt Guruvanich <aekasitt@gmail.com>
2021-08-26 11:24:24 +0700

    Modularized blacklist tests

--- tests/transactions.py
+++ tests/transactions.py
@@ -77,33 +77,3 @@ def test_transfer_hot_potato(admin: Account, user_accounts: List[Account], wrap_
     assert flex_usd.balanceOf(from_addr) == 0
     assert flex_usd.balanceOf(to_addr)   == amount_wei
 
-def test_transfer_while_blacklisted(admin: Account, user_accounts: List[Account], wrap_flex_proxy: flexUSD):
-  print(f'{ BLUE }Transaction Test #4: Transfer amount to account, blacklist it and observe failed transaction due to blacklist.{ NFMT }')
-  amount: int         = 100
-  amount_wei: Decimal = Wei(f'{amount} ether').to('wei')
-  flex_usd: flexUSD   = wrap_flex_proxy
-  ### First Transfer from Admin ###
-  from_addr: str          = admin.address
-  to_addr: str            = user_accounts[0].address
-  txn: TransactionReceipt = flex_usd.transfer(to_addr, amount_wei, { 'from': from_addr })
-  print(f'<Transaction (amount={amount}, txid={txn.txid[:15]}..., from={from_addr[:15]}..., to={to_addr[:15]}...)>')
-  assert flex_usd.balanceOf(user_accounts[0]) == amount_wei
-  ### Blacklist ###
-  blacklist_target: str   = user_accounts[0].address
-  txn: TransactionReceipt = flex_usd.AddToBlacklist(blacklist_target, { 'from': admin })
-  print(txn)
-  assert txn.events['fTokenBlacklist'] is not None
-  print(txn.events)
-  ### Try Transferring Forward ###
-  from_addr: str = user_accounts[0].address
-  to_addr: str   = user_accounts[1].address
-  revert: bool   = False
-  revert_msg: str
-  try:
-    flex_usd.transfer(to_addr, amount_wei, { 'from': from_addr })
-  except VirtualMachineError as err:
-    revert = True
-    revert_msg = err.message
-  assert revert                        == True
-  assert revert_msg                    == 'VM Exception while processing transaction: revert account is blacklisted'
-  assert flex_usd.balanceOf(from_addr) == amount_wei # Balance Unchanged
