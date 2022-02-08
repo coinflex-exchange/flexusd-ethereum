e299f92b31ade6072f59fff9cb1e1d17943d0804
Samuel Isaias <sam.isa@coinflex.com>
2022-02-08 20:22:13 +0800

    refactored all ocurrences of the typo 'D' to 'DECI'

--- contracts/FlexUSD.sol
+++ contracts/FlexUSD.sol
@@ -20,7 +20,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
     require(!initialized, "The library has already been initialized.");
     LibraryLock.initialize();
     admin = msg.sender;
-    multiplier = 1 * D;
+    multiplier = 1 * DECI;
     _totalSupply = _totalsupply;
     _balances[msg.sender] = _totalSupply;
   }
@@ -44,7 +44,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   }
 
   function totalSupply() public override view returns(uint256) {
-    return _totalSupply.mul(multiplier).div(D);
+    return _totalSupply.mul(multiplier).div(DECI);
   }
 
   function setTotalSupply(uint256 inputTotalsupply) external onlyAdmin() {
@@ -52,13 +52,13 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
       inputTotalsupply > totalSupply(),
       "the input total supply is not greater than present total supply"
     );
-    multiplier = (inputTotalsupply.mul(D)).div(_totalSupply);
+    multiplier = (inputTotalsupply.mul(DECI)).div(_totalSupply);
     emit ChangeMultiplier(multiplier);
   }
 
   function balanceOf(address account) public override view returns(uint256) {
     uint256 externalAmt;
-    externalAmt = _balances[account].mul(multiplier).div(D);
+    externalAmt = _balances[account].mul(multiplier).div(DECI);
     return externalAmt;
   }
 
@@ -72,7 +72,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   returns(bool) {
     uint256 internalAmt;
     uint256 externalAmt = amount;
-    internalAmt = (amount.mul(D)).div(multiplier);
+    internalAmt = (amount.mul(DECI)).div(multiplier);
 
     _transfer(msg.sender, recipient, externalAmt);
     return true;
@@ -86,11 +86,11 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   returns(uint256) {
     uint256 internalAmt;
     uint256 maxapproval = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
-    maxapproval = maxapproval.div(multiplier).mul(D);
+    maxapproval = maxapproval.div(multiplier).mul(DECI);
     if (_allowances[owner][spender] > maxapproval) {
       internalAmt = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
     } else {
-      internalAmt = (_allowances[owner][spender]).mul(multiplier).div(D);
+      internalAmt = (_allowances[owner][spender]).mul(multiplier).div(DECI);
     }
 
     return internalAmt;
@@ -189,7 +189,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   ) internal virtual {
     require(sender != address(0), "ERC20: transfer from the zero address");
     require(recipient != address(0), "ERC20: transfer to the zero address");
-    uint256 internalAmt = externalAmt.mul(D).div(multiplier);
+    uint256 internalAmt = externalAmt.mul(DECI).div(multiplier);
     _balances[sender] = _balances[sender].sub(
       internalAmt,
       "ERC20: transfer internalAmt exceeds balance"
@@ -205,7 +205,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   ispaused()
   returns(bool) {
     uint256 externalAmt = amount;
-    uint256 internalAmt = externalAmt.mul(D).div(multiplier);
+    uint256 internalAmt = externalAmt.mul(DECI).div(multiplier);
     _mint(mintTo, internalAmt, externalAmt);
     return true;
   }
@@ -230,7 +230,7 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
   returns(bool) {
     uint256 internalAmt;
     uint256 externalAmt = amount;
-    internalAmt = externalAmt.mul(D).div(multiplier);
+    internalAmt = externalAmt.mul(DECI).div(multiplier);
 
     _burn(burnFrom, internalAmt, externalAmt);
     return true;
@@ -260,11 +260,11 @@ contract FlexUSD is FlexUSDStorage, Context, IERC20, Proxiable, LibraryLock {
     require(spender != address(0), "ERC20: approve to the zero address");
     uint256 internalAmt;
     uint256 maxapproval = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
-    maxapproval = maxapproval.div(multiplier).mul(D);
+    maxapproval = maxapproval.div(multiplier).mul(DECI);
     if (externalAmt > maxapproval) {
       internalAmt = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
     } else {
-      internalAmt = externalAmt.mul(D).div(multiplier);
+      internalAmt = externalAmt.mul(DECI).div(multiplier);
     }
     _allowances[owner][spender] = internalAmt;
     emit Approval(owner, spender, externalAmt);
